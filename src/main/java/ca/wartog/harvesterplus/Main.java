9f508c7a7a5c695e11e2c4d61184a155f14d23dd
sam123114 <samuelbou2000@gmail.com>
2020-06-04 18:53:08 -0400

    Added instance member and it's getter

--- src/main/java/ca/wartog/harvesterplus/Main.java
+++ src/main/java/ca/wartog/harvesterplus/Main.java
@@ -4,10 +4,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin{
 	
+	public static Main instance;
+	
 	@Override
 	public void onEnable() {
-		
+		instance = this;
 		
 	}
 
+	
+	public static Main getInstance() {
+		return instance;
+	}
 }
